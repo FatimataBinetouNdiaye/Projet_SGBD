0e8227259f7e6ea46e904e4fa3d4c91f86949225
mamjarra <mamejarra098@gmail.com>
2025-03-07 22:58:21 +0000

    Configuration MySQL et initialisation de la base de données

---
+++ backend/plateforme_eval/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'projet_BD',
+        'USER': 'root', 
+        'PASSWORD': "passer123@",
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
